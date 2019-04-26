6a99bf4ad6c83b097691ff3b1278c878916072e5
cristiansilva143 <cristiansilva143@gmail.com>
2019-04-26 18:55:59 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,30 +2,6 @@ const express = require('express');
 const app = express();
 const request = require('request');
 const async = require('async');
-const { Client } = require('pg');//importa el cliente de la BD
-
-
-const connectionData = { //abre la conexion con postgressql
-    user: 'cristian',
-    host: 'localhost',
-    database: 'API',
-    password: '123',
-    port: 5432,
-  }
-  const client = new Client(connectionData)
-
-
-       ///consulta////
-  client.connect()
-client.query('SELECT * FROM table')
-    .then(response => {
-        console.log(response.rows)
-        client.end()
-    })
-    .catch(err => {
-        client.end()
-    })
-
 ////////////////////////////////////////////////////////////
 
 
@@ -34,13 +10,39 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/upcoming', (req, res) => {
-    //res.setHeader('', '*');
-    async.times(1, (i, callback) => {
-    var options = {
-        url: 'https://api.themoviedb.org/3/movie/upcoming?api_key=ee6206f4b5d4aa4e800d0117d6724efb',
+
+app.get('/api/football', function(req, res, next) {
+    request({
+      //uri: 'https://allsportsapi.com/api/football/?&met=Teams&teamId=2616&APIkey=3181aba25e0ededb5fa60883bd351da54315e3395abfbee8ab8cf6f768c63751',
+      uri: 'https://allsportsapi.com/api/football/?met=Fixtures&APIkey=3181aba25e0ededb5fa60883bd351da54315e3395abfbee8ab8cf6f768c63751&from=2018-05-23&to=2019-03-01',
+      /*qs: {
+        api_key: '123456',
+        query: 'World of Warcraft: Legion'
+      }*/
+    }).pipe(res);
+
+    /*request(options, (error, response, body) => {
+        var result = JSON.parse(body)
+        var data = result.results;
+        callback(null, data);
+    });
+    }, (err, results) => {
         
-    } 
+        res.status(200).json(results);
+    }).done(function(response) {
+        // do something with the response, e.g. isolate the id of a linked resource   
+        console.log(response);
+      }); 
+
+app.get('api/football/?&met=Teams', (req, res) => {
+    console.log('api');
+    //res.setHeader('', '*');
+    /*async.times(1, (i, callback) => {
+    var options = {
+       url:'https://allsportsapi.com/api/football/?&met=Teams&teamId=2616&APIkey=3181aba25e0ededb5fa60883bd351da54315e3395abfbee8ab8cf6f768c63751',
+        //url: 'https://api.themoviedb.org/3/movie/upcoming?api_key=ee6206f4b5d4aa4e800d0117d6724efb',
+        
+    }
     request(options, (error, response, body) => {
         var result = JSON.parse(body)
         var data = result.results;
@@ -49,12 +51,11 @@ app.get('/upcoming', (req, res) => {
     }, (err, results) => {
         
         res.status(200).json(results);
-    });
+    }).done(function(response) {
+        // do something with the response, e.g. isolate the id of a linked resource   
+        console.log(response);
+    });*/
 });
-
-app.get('/auth/login', (req, res) => {
-    return {};
-    });//prueba login
 
 
 //servidor
@@ -64,15 +65,8 @@ app.listen('8010', () => {
 
 //HTTP : POST
 
-app.post('/upcoming', (req, res) => {
-    /*const id = movies.length + 1;
-    const { Id, original_language, original_title } = req.body;
-    const newMovie = { ...req.body, id };
-    if (id && Id && original_language && original_title) {
-        movies.push(newMovie);
-        console.log(movies);
-        res.json(movies);*/
+app.post('/api/football', (req, res) => {
+   
         res.send("Ok post!");
     }
     );
-
